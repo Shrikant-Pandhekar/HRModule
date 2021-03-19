2bdd88891682ba1d2e0f2de53e14b7a6dda0b903
Ashvin Kasture <kashvin525@gmail.com>
2021-03-19 22:27:36 +0530

    added database configuration according to deployment

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hrmodule');
+define('DB_SERVER','remotemysql.com');
+define('DB_USER','wkqgOIzEHy');
+define('DB_PASS' ,'v695L71jtf');
+define('DB_NAME', 'wkqgOIzEHy');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 
